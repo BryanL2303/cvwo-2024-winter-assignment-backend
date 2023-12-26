73ef532fc35922082c9f10c2edd7b9b3f3ceed27
BryanL2303 <bryanlee0033@gmail.com>
2023-12-26 20:49:30 +0800

    Fixed type in function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
 
 	def decode_token(token)
 		begin
-			JWT.decode(token, 'secretKey', true, algorithm: 'HS256')
+			JWT.decode(token, 'secretkey', true, algorithm: 'HS256')
 		rescue JWT::DecodeError
 			nil
 		end
