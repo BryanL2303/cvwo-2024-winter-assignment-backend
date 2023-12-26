0cb02a60c63d277bd8ddcc9b7cbc2a6bea5190c7
BryanL2303 <bryanlee0033@gmail.com>
2023-12-26 20:24:04 +0800

    Fixing bug in posts controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
-    
-    # Create new users with the following params
-    # username: string
-    # password: string
-    #
-    # renders: 
-    # 0 - success
-    # 1 - account username already exists
-    # 2 - error interacting with database
-    def signup        
+	# Create new users with the following params
+	# username: string
+	# password: string
+  	#
+  	# renders: 
+  	# 0 - success
+  	# 1 - account username already exists
+  	# 2 - error interacting with database
+  	def signup        
 		findUser = User.find_by(username: params[:username])
 
 		if findUser == nil
@@ -22,47 +21,47 @@ class UsersController < ApplicationController
 		else
 			render json: {status: 1}
 		end
-    end
+	end
 
-    # Log in with the following params
-    # username: string
-    # password: string
-    # 
-    # renders status:
-    # 0 - success
-    # 1 - username/password is wrong
-    # 
-    # On success also renders JWT
-    def login
-        user = User.find_by(username: params[:username])
+  # Log in with the following params
+  # username: string
+  # password: string
+  # 
+  # renders status:
+  # 0 - success
+  # 1 - username/password is wrong
+  # 
+  # On success also renders JWT
+  def login
+    user = User.find_by(username: params[:username])
 
-		if user == nil
-			render json: {status: 1}
+	if user == nil
+		render json: {status: 1}
+	else
+		if user['password'] == params[:password]
+			token = encode_token({user_id: user.id})
+			render json: {status: 0, token: token}
 		else
-			if user['password'] == params[:password]
-				token = encode_token({user_id: user.id})
-				render json: {status: 0, token: token}
-			else
-				render json: {status: 1}
-			end
+			render json: {status: 1}
 		end
-    end
+	end
+  end
 
-    # Log in with the following params
-    # token: string
-    # 
-    # renders status:
-    # 0 - success
-    # 1 - user does not exist
-    # 
-    # On success renders username
-    def autoLogin
-        user = authorised_user(params[:token])
+  # Log in with the following params
+  # token: string
+  # 
+  # renders status:
+  # 0 - success
+  # 1 - user does not exist
+  # 
+  # On success renders username
+  def autoLogin
+    user = authorised_user(params[:token])
         
-        if user == nil
-			render json: {status: 1}
-		else
-			render json: {status: 0, username: user.username}
-		end
-    end
+    if user == nil
+		render json: {status: 1}
+	else
+		render json: {status: 0, username: user.username}
+	end
+  end
 end
