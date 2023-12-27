7931cce796fc22535ef9757c7be06e13bac19d8b
BryanL2303 <bryanlee0033@gmail.com>
2023-12-28 00:27:25 +0800

    Added route to function in labels

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post "/login" => "users#login"
   post "/auto_login" => "users#autoLogin"
   post "/create_label" => "labels#createLabel"
+  post "/get_labels" => "labels#getLabels"
   post "/create_post" => "posts#createPost"
   post "/get_posts" => "posts#getAllPosts"
   post "/get_post" => "posts#getPost"
