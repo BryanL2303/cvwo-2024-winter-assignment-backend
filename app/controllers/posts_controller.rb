9d6bcfc85f46e3bf1739b811fc51df9338c50228
BryanL2303 <bryanlee0033@gmail.com>
2023-12-29 18:12:58 +0800

    Testing joining of tables p2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,7 +49,7 @@ class PostsController < ApplicationController
         #posts = Post.all.joins(:labels).order('id')
         posts = Post.all.order('id')
 
-        render json: {status: 0, posts: posts}
+        render json: {status: 0, posts: posts, labels: posts.labels}
     end
 
     def getPost
